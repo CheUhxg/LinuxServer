02279492c96fab48c14e5191aa8c5bf2e750cb1a
CheUhxg <3193079152@qq.com>
2022-04-28 17:13:18 +0800

    remove useless headers

--- sendfile/main.cpp
+++ sendfile/main.cpp
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <fcntl.h>
-#include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
